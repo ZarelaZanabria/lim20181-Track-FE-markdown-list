dab4b598cf1606e1188e824e49c8c7d66dd02cdc
Zarela Zanabria <zarelixzana@gmail.com>
2018-08-23 17:35:18 -0500

    Promesas

--- example/zarela/README33.md
+++ example/zarela/README33.md
@@ -1,1 +1,3 @@
 - [Acerca de Node.js - Documentación oficial](https://nodejs.org/es/about/)
+
+- [Acerca de Node.js - Documentación oficial](https://nodejs.sss)
--- index.js
+++ index.js
@@ -2,30 +2,21 @@
 //Desde este archivo debes exportar una función (mdLinks)
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const fetch = require('node-fetch');
+
 //Array en donde se va almacenar los links del archivo markdown
 let links = [];
-
 //--------------------------------------------------------------------FUNCCION PARA OBTENER LOS LINKS DE UN ARCHIVO MARKDOWN
 const readFileMarkdown = path => {
   let linksMd = [];
   const readMarkdown = fs.readFileSync(path, 'utf-8');
-  /*   i por lo que las mayúsculas serán ignoradas
-  Mediante una expresión regular le decimos que extraiga los linksMd node
-  que esten dentro de los corchetes y los parentesis */
   const RegExpLink = /!*\[(.*)\]\((.*)\)/gi;
   let urlLinks = readMarkdown.match(RegExpLink);
-  // Solo lo que se encuentre dentro de los corchetes
-  /*    const textLink = /\[(\w+.+?)\]/gi ; */
   const textLink = /\[(.*)\]/gi
-  // Solo lo que se encuentre dentro de los parentesis
   const urlLink = /\]\((.*?|(https?|http?|ftp):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-]))\)/gi;;
   for (let i = 0; i < urlLinks.length; i++) {
-    /* console.log(urlLinks[i]) */
-    //subtring permite tener una cantidad de caracteres de una posicion (1,5) hasta otra 
-    /* Este codigo permite sacar los corchetes o parentesis de los link o text
-     match(urlLink)[0].match(urlLink)[0].substring(1, urlLinks[i].match(urlLink)[0].length - 1);   */
     let textLinkMarkdown = urlLinks[i].match(textLink)[0].substring(1, urlLinks[i].match(textLink)[0].length - 1);
     let urlLinkMarkdown = urlLinks[i].match(urlLink)[0].match(urlLink)[0].substring(2, urlLinks[i].match(urlLink)[0].length - 1);
     //Este codigo permite agregar al array un objetos con las siguientes propiedades
@@ -33,20 +24,11 @@ const readFileMarkdown = path => {
       text: textLinkMarkdown,
       href: urlLinkMarkdown,
       file: path
-
     });
   }
   return linksMd; // linksMd guardamos el array de objetos con los linksMd
 }
 
-//-----------------------------------------------------------------------------------------FUNCION PARA CONVETIR UN RUTA RELATIVA A ABSOLUTA
-const convertRelativeAbsolute = fileName => {
-  if (path.isAbsolute(fileName)) {
-    return fileName = fileName.toString();
-  } else {
-    return fileName = process.cwd().toString() + fileName.substr(fileName.indexOf('/'));
-  }
-}
 //-------------------------------------------------------------------------------------------FUNCION VERIFICA SI ES UN DIRECTORIO O CARPETA
 const checkIfFileOrFolder = path => fs.stat(path, (error, stats) => {
   if (error) {
@@ -56,7 +38,6 @@ const checkIfFileOrFolder = path => fs.stat(path, (error, stats) => {
   }
   else {
     if (stats.isDirectory()) {
-
       fs.readdir(path, 'utf8', (error, files) => {
         if (error) {
           console.log(error);
@@ -74,64 +55,77 @@ const checkIfFileOrFolder = path => fs.stat(path, (error, stats) => {
         los archivos markdown encontrados */
       /* links = links.concat(readFile(path)) */
       links = links.concat(readFileMarkdown(path))
-       console.log (links) 
-
+       /* console.log(links)  */
     }
   }
+
 });
 
-//-----------------------------------------------------------FUNCION PARA VALIDAR LINKS ASINCRONO CON NODE-FECTH 
-
-const validateLinks = (linksMark, callback) => {
-  let linkMarkdValidate = [];
-
-  
-  linksMark.map(element => console.log (element.href));
-
-  // esperando la respuesta de todas las promesas
-  setTimeout(() => callback(linkMarkdValidate), 5000);
-};
-
-/* 
-fetch('http://jsonplaceholder.typicode.com/posts/1')
-.then(function(response){
-    return response.json();
-})
-.then(function(json){
-    console.log(json);
-});
- */
-/* module.exports = mdLinks; */
 
 //---------------------------------------------------------------FUNCION PARA VALIDAR UN LINK CALLBACK
-const  validateLinkOk=(url,callback)=> {
+
+
+// => [{ href, text, file, status, ok }]
+
+/* const validatelinkASYN = (arrayLinks)=>{
+  return new Promise ((resolve, reject)=>{
+    arrayLinks.forEach(element => {
+      fetch (element.href)
+      .then (response => {
+        console.log (response.status);
+      }).catch(error)      
+    });
+  });
+
+}  */
+
+/* const validateLink = (url, callback) => {
   var req = new XMLHttpRequest();
   req.open("GET", url, true);
-  req.addEventListener("load", () =>{
+  req.addEventListener("load", () => {
     if (req.status >= 200 && req.status < 400) {
-      callback(null, { url })
+      callback(null, { url})
     } else {
       console.error(req.status + " " + req.statusText);
     }
   });
-  req.addEventListener("error", ()=>{
+  req.addEventListener("error", () => {
     callback({ error: true, url })
   });
   req.send(null);
-} 
+}  */
+
+const miPrimeraPromise = new Promise((resolve, reject) => {
+  // Llamamos a resolve(...) cuando lo que estabamos haciendo finaliza con éxito, y reject(...) cuando falla.
+  // En este ejemplo, usamos setTimeout(...) para simular código asíncrono. 
+  // En la vida real, probablemente uses algo como XHR o una API HTML5.
+  setTimeout(function () {
+    resolve(links); // ¡Todo salió bien! // me esta retornando mi array 
+  }, 1000);
+});
+
+miPrimeraPromise.then((successMessage) => {
+ /*  successMessage.forEach(element => {
+    validateLink(element.href, (error, respuesta) => {
+       if (error) {
+        console.log(error.url + '  fail');
+      } else {
+        element.textstatus = 'ok',
+        element.status = respuesta.status,
+         console.log(respuesta.url + ' ok  ');
+         console.log(element)
+      }
+    }); */
+
+    // succesMessage es lo que sea que pasamos en la función resolve(...) de arriba.
+    // No tiene por qué ser un string, pero si solo es un mensaje de éxito, probablemente lo sea.
+     console.log(successMessage); 
+  /* }); */
+}); 
+
+  checkIfFileOrFolder('./example');
 
 
 
-/* linkFileMarkdown.forEach(element => {  
-  validateLink(element.href, (error, respuesta) => {
-    if (error) {
-      console.log( error.url + 'fail' );      
-    } else{
-      console.log( respuesta.url + ' ok  ');      
-    }
-  }); 
-   
-});*/
-/* 
-console.log(checkIfFileOrFolder('F:\\Zarela Data\\NPm\\example')) */
-console.log(checkIfFileOrFolder('./example'))
+//-----------------------------------------------------------FUNCION PARA VALIDAR LINKS ASINCRONO CON NODE-FECTH 
+
